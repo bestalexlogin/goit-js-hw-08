bebfcc068602a8d77b46e3f202c014b4eb8cdc52
bestalexlogin <never.fade.ever@gmail.com>
2023-11-14 02:19:59 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ original, preview, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
