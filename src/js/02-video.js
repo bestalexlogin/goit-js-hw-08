b203a7fac67978b32df1e07993e4d8ae14f04aaf
bestalexlogin <never.fade.ever@gmail.com>
2023-11-14 12:05:36 +0200

    com

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(TIME_KEY, JSON.stringify(data));
+}
+
+let timeData = JSON.parse(localStorage.getItem(TIME_KEY)) ?? { seconds: 0 };
+player.setCurrentTime(timeData.seconds);
